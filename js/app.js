e352aef90f2923d49f0ea05dd9eac82c8c276fa8
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-09-10 18:33:47 +0100

    added more events

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var ShoppingBasket= [];
 var ans="";
 var wallet = 20.0;
 var cost = 0;
+var bool=false;
 // intro
 alert("Welcome to control flow lab, lets go to the store");
 var item = prompt("what would you like to buy? (Type only first letter)\n[B]ananas\n[A]pples\n[D]rink\n[C]hicken");
@@ -25,7 +26,18 @@ switch (item) {
     }else {
       alert("I have no idea what you want. Just reload to restart ok?");
       location.reload();
-      }
+    };
+    ans = prompt("you notice a £20 note on the floor. Do you pick it up?\n[y]es\n[n]o");
+    ans = ans.toLowerCase();
+    if (ans ==="y"){
+      alert("You picked up the £20.")
+      wallet = wallet +20;
+    } else if (ans ==="n") {
+      alert("you left the note on the floor. someone else picked it up");
+    }else {
+      alert("I have no idea what you want. Just reload to restart ok?");
+      location.reload();
+    };
     break;
   case "c":
     alert("Chicken is in aisle 2");
@@ -41,12 +53,27 @@ switch (item) {
     }else {
       alert("I have no idea what you want. Just reload to restart ok?");
       location.reload();
-      }
+    };
+    alert("You see a crying child in the middle of the aisle. It looks like she is lost");
+    ans = prompt("Help her find her parents?\n[Y]es\n[N]o");
+    ans =ans.toLowerCase();
+    if (ans ==="y"){
+      alert("you help the girl find her parents. they are so happy they give you £50!");
+      wallet = wallet +50;
+      console.log("wallet " +wallet);
+    }else if (ans="n") {
+      alert("You Monster.");
+    }else{
+      alert("I have no idea what you want. Just reload to restart ok?");
+      location.reload();
+    }
+
     break;
   case "d":
     alert("The Drinks are in aisle 3");
     alert("You go to Aisle 3 and find the Drinks. They are 60p");
     ans = prompt("will you pick up a Drink?\n[y]es\n[n]o");
+    ans=ans.toLowerCase();
     if (ans === "y"){
       alert("You pick the Drink up");
       ShoppingBasket.push("Drink");
@@ -56,25 +83,62 @@ switch (item) {
     }else {
       alert("I have no idea what you want. Just reload to restart ok?");
       location.reload();
+    };
+
+    alert("You notice a Pyramid of Peanuts. They cost £2.50");
+    ans= prompt("take a can from the [t]op or the [b]ottom");
+     ans = ans.toLowerCase();
+    if (ans ==="t") {
+      alert("you get some peanuts");
+      ShoppingBasket.push("Peanuts");
+      console.log(ShoppingBasket);
+    }else if (ans==="b") {
+      alert("the pyramid Collaspes");
+      alert("a worker sees what you did and starts to Shout at you.");
+      ans = prompt("Do you shout Back?\n[Y]es\n[N]o");
+      ans = ans.toLowerCase();
+      if (ans==="y") {
+        alert("The Manager comes over and kicks you out. Restart");
+        location.reload();
+      }else if (ans==="n") {
+        alert("you apologize and give him £20");
+        wallet = wallet - 20;
+        ShoppingBasket.push("Peanuts");
+      }else {
+        alert("I have no idea what you want. Just reload to restart ok?");
+        location.reload();
       }
+
+    }else{
+      alert("I have no idea what you want. Just reload to restart ok?");
+      location.reload();
+    }
+
     break;
   default:
   alert("you look for hours but you can't find it and give up. time to reload.");
   location.reload();
-}
+};
 // Checkout
 alert("with the items you want, you go to the cashier");
 alert("The Cashier scans your items");
 
-if(ShoppingBasket[0] == "Apple/Banana") {
+if(ShoppingBasket[0] === "Apple/Banana") {
   cost = cost + 1.50;
   console.log(`cost is £${cost}`);
 } else if(ShoppingBasket[0]==="Chicken"){
   cost = cost + 3;
-  console.log(`cost is ${cost}`);
+  console.log(`cost is £${cost}`);
 } else if(ShoppingBasket[0]==="Drink"){
   cost = cost + .60;
-}
+};
+
+if(ShoppingBasket[1]!== "Peanuts"){
+  console.log("no Peanuts");
+} else{
+  cost = cost +2.50;
+  console.log(`cost is £${cost}`);
+};
 
 alert("The cashier asks you to pay");
 if (wallet >= cost){
@@ -83,4 +147,7 @@ if (wallet >= cost){
   console.log(`wallet is £${wallet}`);
   alert(`you have £${wallet} left`);
   alert("you leave the store. Goodbye!!");
+}else {
+  alert("You dont have enough!");
+  alert("they kick you out. Restart");
 }
